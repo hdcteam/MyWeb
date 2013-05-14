dda33236c74a2e505ead38cc1b7ec7b4a8965148
Mai Hoang Hai <hoanghai27@gmail.com>
2013-05-14 13:16:24 +0700

    Add MapRoute

--- MyWeb/App_Start/RouteConfig.cs
+++ MyWeb/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MyWeb
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Default2",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index"}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
